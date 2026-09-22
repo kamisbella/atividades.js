1fd1ed51da992d16a4512a34e1d6985b98d6a272
𝕸𝖎𝖑𝖆 𝕻𝖆𝖚𝖑𝖎𝖘𝖙𝖆 ₊˚⊹ ᰔ <kamilaf0709@gmail.com>
2026-09-22 16:33:41 -0300

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,15 @@
+// 
+
+
+
+
+
+
+
+function  calcularAreaRetangulo() {
+    let base= Number(prompt("Digite o valor da base:"))
+    let altura= Number(prompt("Digite o valor da altura:"))
+    let area= base*altura
+    return alert ("A area do retangulo é: "+ area)
+}
+ calcularAreaRetangulo()
